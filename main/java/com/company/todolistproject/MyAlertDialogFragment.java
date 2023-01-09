a5f759bdf2e6dc4528859aaf51bb3da2d7dcf563
Cezary92 <105557061+Cezary92@users.noreply.github.com>
2023-01-09 21:20:32 +0100

    Zad. 3
    (Not work, add class AlertDialogFragmentu but not work)

---
+++ main/java/com/company/todolistproject/MyAlertDialogFragment.java
@@ -0,0 +1,34 @@
+/*package com.company.todolistproject;
+
+import android.app.Dialog;
+import android.app.AlertDialog;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class MyAlertDialogFragment extends AlertDialogFragment implements DialogInterface.OnClickListener  {
+
+
+  @Override
+  public Dialog onCreateDialog(Bundle savedInstanceState) {
+    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+    builder.setTitle("Delete item");
+    builder.setMessage("Do you want delete?");
+    builder.setPositiveButton("YES", this);
+    builder.setNegativeButton("NO", this);
+    return builder.create();
+  }
+
+  @Override
+  public void onClick(DialogInterface dialog, int which) {
+    switch (which) {
+      case DialogInterface.BUTTON_POSITIVE:
+        break;
+      case DialogInterface.BUTTON_NEGATIVE:
+        break;
+    }
+  }
+
+
+}
+*/
+
